0359bc618ace66d21191ae8febbf443ad548477e
Leonardo <leonardotavares656@gmail.com>
2025-07-23 15:54:27 -0300

    Adicionando conteúdo estático a página principal do usuário

--- Frontend/Scripts/home-page.js
+++ Frontend/Scripts/home-page.js
@@ -19,6 +19,9 @@ document.addEventListener("DOMContentLoaded", async()=>{
             window.location.href = '/login'
         }
 
+        const usuario = await res.json()
+        document.getElementById("name_user").innerHTML = usuario.name
+
     }catch(err){
         console.error(err)
         window.location.href = '/login'
